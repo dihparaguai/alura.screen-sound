e3d9a412e8c968015718802564bbdc668aa14b1c
Dih Paraguai <diego.paraguai.dp@gmail.com>
2024-01-09 22:38:20 -0300

    ambos os codigos em Program.cs presentes, sendo o antigo comentado

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -13,3 +13,34 @@ Podcast podcast = new("Podcast Especial", "Dieguito");
 podcast.AdicionarEpsodio(ep1);
 podcast.AdicionarEpsodio(ep2);
 podcast.ExibirDetalhes();
+
+/* CÓDIGO ANTIGO
+
+Banda queen = new Banda("Queen");
+
+Album albumQueen = new Album("A night at the opera");
+
+Musica musica1 = new Musica(queen, "Love of my Life")
+{
+    Duracao = 213,
+    Disponivel = true,
+
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354, // inicializadores
+    Disponivel = false,
+};
+
+
+albumQueen.AdicionarMusica(musica1);
+albumQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumQueen);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumQueen.ExibirAlbum();
+queen.ExibirDiscografia();
+
+*/
